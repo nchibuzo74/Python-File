76d07b76679cfc1d962650170e508b73345e7422
Chibuzo Nwankwo <nwankwochibuzosamuel24@gmail.com>
2025-07-03 14:54:07 +0100

    New File Created

---
+++ Zero To Hero Class/Day_2_Assignment.py
@@ -0,0 +1,56 @@
+"""Question 1: Ask the user for their name and favourite colour, then print:
+Hello [name], your favourite colour is [colour] ! (Use f-string)
+"""
+
+#Solution:
+your_name = input("Enter your name: ")
+favourite_colour = input("Enter your favourite colour: ")
+
+print(f'Hello {your_name}, your favourite colour is {favourite_colour}!')
+
+
+"""
+Question 2: Prompt for a number. Print whether it's:
+- Positive
+- Negative
+- Zero
+"""
+
+#Solution:
+number = int(input("Enter any number: "))
+
+if number > 0:
+    print(f'The number is postive: {number}')
+
+elif number == 0:
+    print(f'The number is Zero: {number}')
+
+else:
+    print(f'The number is negative {number}')
+
+
+"""
+Write a program to calculate a customer's total bill based on: 
+- Quantity
+- Price
+- Print the result using f-string.
+"""
+#Solution:
+"""
+Thought Process: 
+i. Create a variable to accept user input:
+ii. Create another variable to compute the calculation
+"""
+Quantity = int(input("Enter the quantity bought: "))
+Price = int(input("Enter the sales amount of the product: "))
+
+total_bill = Quantity * Price
+
+print(f'The customer total bill is {total_bill}')
+
+
+
+"""
+Prompt the user to enter their score and:
+print grade A (90-100), B (80-89), C (70-79), F (below 70)
+"""
