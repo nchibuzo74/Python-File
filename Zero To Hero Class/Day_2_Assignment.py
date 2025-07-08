805a2a6a0c6d0f227c3122a5e56f64ced05e30b1
Chibuzo Nwankwo <nwankwochibuzosamuel24@gmail.com>
2025-07-08 21:58:11 +0100

    File Added

--- Zero To Hero Class/Day_2_Assignment.py
+++ Zero To Hero Class/Day_2_Assignment.py
@@ -432,6 +432,39 @@ if __name__ == "__main__":
     password = input("Enter password: ")
     
     if validate_credentials(username, password):
-        print("Credentials are valid!")
+        print(f'CLogin successful')
     else:
-        print("Credentials are invalid!")
+        print(f'Invalid credentials')
+
+
+"""
+Question 19: Write a program to determine the price of a movie ticket. 
+The base price is $12 for adults (age 18 and over) and $8 for children. 
+If it's a weekend, add $2 to the price. Ask the user for their age and if it is a weekend 
+(e.g., they can enter 'yes' or 'no').
+"""
+#Solution:
+#Thought Process:
+#- Two approach/method can be used to achieve this. A program can be created without a function or a function
+#-Create a variable to accept user input
+
+#Method 1:
+enter_your_age = int(input("How old are you: "))
+is_weekend = input("Yes/No: ").strip().title()
+adult_ticket = 12
+children_ticket = 8
+weekend_price = 2
+
+#Identify the person age and weekend status
+if enter_your_age >= 18 and is_weekend == "No":
+    print(f'It is a weekday and Adult movie ticket price is {adult_ticket}')
+
+elif enter_your_age < 18 and is_weekend == "No":
+    print(f'It is a weekday and Children movie ticket price is {children_ticket}')
+
+elif enter_your_age >= 18 and is_weekend == "Yes":
+    total_price = adult_ticket + weekend_price
+    print(f'The weekend movie ticket price is {total_price}')
+
+else:
+    print(f'The weekend movie ticket price is {children_ticket + weekend_price}')
