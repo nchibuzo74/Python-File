e48d36ef3a275beecec6a6ac8dee94fa5e59d3e5
Chibuzo Nwankwo <nwankwochibuzosamuel24@gmail.com>
2025-07-10 10:55:06 +0100

    Question 1

---
+++ Zero To Hero Class/Assignment_2/Question_1.py
@@ -0,0 +1,9 @@
+"""Question 1: Ask the user for their name and favourite colour, then print:
+Hello [name], your favourite colour is [colour] ! (Use f-string)
+"""
+
+#Solution:
+your_name = input("Enter your name: ")
+favourite_colour = input("Enter your favourite colour: ")
+
+print(f'Hello {your_name}, your favourite colour is {favourite_colour}!')
