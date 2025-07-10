ac0feb9ef7f5e654ce81dbf04bc85b82e4eb2782
Chibuzo Nwankwo <nwankwochibuzosamuel24@gmail.com>
2025-07-10 11:22:42 +0100

    Question 7

---
+++ Zero To Hero Class/Assignment_2/Question_7.py
@@ -0,0 +1,27 @@
+"""
+Question 7: Write a Python program to find the least common multiple (LCM) of two positive 
+integers. enter by the user from the terminal.
+"""
+
+#Solution:
+"""
+Thought Process:
+- Create a variable taking user input
+- Create a variable to find the greater number between two numbers
+- Check multiples if greater number is divisible by both numbers.
+"""
+# Take input from the user
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+
+# Find the greater number
+greater_number = max(number1, number2)
+
+# Keep checking multiples of the greater number until divisible by both
+while True:
+    if greater_number % number1 == 0 and greater_number % number2 == 0:
+        lcm = greater_number
+        break
+    greater_number += 1
+
+print(f"The LCM of {number1} and {number2} is {lcm}")
