34b1b95dad787eacc844224def8b00dadee8ba73
Chibuzo Nwankwo <nwankwochibuzosamuel24@gmail.com>
2025-07-10 11:38:08 +0100

    File Added

---
+++ Zero To Hero Class/Assignment_2/Question_8.py
@@ -0,0 +1,20 @@
+"""
+Question 8: Write a Python program to compute the future value of a specified principal 
+amount, rate of interest, and number of years. (calculate simple interest based on 
+user input)
+"""
+
+#Solution:
+"""
+Thought Process: 
+- The formula for simple interest is (P * R * T) / 100
+- Create a variable accepting user input - principal, rate and time
+- Create a varaible to compute the calculation
+"""
+Principal = int(input("Enter the Principal amount: "))
+Rate = int(input("Enter the Rate (%): "))
+Time = int(input("Enter the Time (in years): "))
+
+SI = (Principal * Rate * Time) / 100
+
+print(f'The Simple Interest is {SI}')
