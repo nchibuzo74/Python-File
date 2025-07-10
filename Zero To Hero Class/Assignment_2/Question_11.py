53bfe593167fa1de0a2e03ef68aaace147114767
Chibuzo Nwankwo <nwankwochibuzosamuel24@gmail.com>
2025-07-10 11:46:16 +0100

    Question 11

---
+++ Zero To Hero Class/Assignment_2/Question_11.py
@@ -0,0 +1,12 @@
+#Question 11:  Write a Python program to calculate the hypotenuse of a right angled triangle.
+
+#Solution:
+#Thought Process: The formula for calculating the hypotenuse is: c = sqrt(a^2 + b^2)
+# - Create a variable to accept user input for the two sides of the triangle
+
+first_number = int(input("Enter the first side of the triangle: "))
+second_number = int(input("Enter the second side of the triangle: "))
+
+hypotenuse = (((first_number ** 2) + (second_number ** 2)) ** 0.5)
+
+print(f'The hypotenuse of the right angled triangle is {hypotenuse}')
