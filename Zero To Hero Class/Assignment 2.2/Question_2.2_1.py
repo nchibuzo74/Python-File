acf2b63db30dba617fc6bcfee7f123f5bc465f58
Chibuzo Nwankwo <nwankwochibuzosamuel24@gmail.com>
2025-07-19 20:59:20 +0100

    File Added

---
+++ Zero To Hero Class/Assignment 2.2/Question_2.2_1.py
@@ -0,0 +1,35 @@
+"""
+1.Write a Python program to construct the following pattern, using a nested for loop. 
+*  
+* *  
+* * *  
+* * * *  
+* * * * *  
+* * * *  
+* * *  
+* *  
+* 
+"""
+
+#Solution:
+#Thought Process: 
+# Create a varaible to store the list of stars in memory
+# Use a for statement to loop the increasing stars within the range of stars
+#Print the stars
+#Loop again with the for statement to loop the decreasing stars within the range of stars
+#Print the stars
+
+# Number of rows in the upper half of the pattern
+n = 5
+
+# Upper half (increasing stars)
+for i in range(1, n + 1):
+    for j in range(i):
+        print("*", end=" ")
+    print()  # Move to the next line
+
+# Lower half (decreasing stars)
+for i in range(n - 1, 0, -1):
+    for j in range(i):
+        print("*", end=" ")
+    print()  # Move to the next line
