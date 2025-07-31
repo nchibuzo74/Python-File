94fcf78313f0d42bd454a4df4c50844461d8a5a7
Chibuzo Nwankwo <nwankwochibuzosamuel24@gmail.com>
2025-07-31 16:01:19 +0100

    File Added

---
+++ Zero To Hero Class/File Handling/Lecture.py
@@ -0,0 +1,15 @@
+# File Handling Operations
+"""
+write - write to a file
+read - read from a file
+append - append to a file
+delete - delete a file
+rename - rename a file
+move - move a file
+copy - copy a file
+"""
+
+import os
+
+# Open a file (create if not exists)
+file = open('')
